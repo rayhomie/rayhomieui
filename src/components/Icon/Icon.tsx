5579b7cffb334f51645906b217152eea0aadeb53
leihao <leihao@xiaoduotech.com>
2020-10-09 11:18:23 +0800

    Icon start

---
+++ src/components/Icon/Icon.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import classNames from 'classnames'
+
+interface IconProps {
+
+}
+const Icon: React.FC<IconProps> = (props) => {
+    return (
+        <>
+
+        </>
+    )
+}
